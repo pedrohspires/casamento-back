f4e73abd709256440ff13293ec00ec1d01a0a46e
Pedro Henrique <pedropires2603@gmail.com>
2024-12-02 22:32:51 -0300

    First migration

---
+++ casamento-back/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace casamento_back.Entities
+{
+    public class BaseEntity
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("data_cadastro")]
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ casamento-back/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Testes
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configurações de entidades
+            //modelBuilder.Entity<Produto>().HasKey(u => u.Id);
+        }
+    }
+}
